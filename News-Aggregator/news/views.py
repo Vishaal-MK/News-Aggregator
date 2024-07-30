dff0883dd8cc5c5a825b1709913ab31480d2b522
Vishaal MK <vishaalmk@pivotx.co>
2024-07-30 23:35:55 +0530

    Updated scraping code

--- News-Aggregator/news/views.py
+++ News-Aggregator/news/views.py
@@ -15,7 +15,7 @@ def scrape(request, name):
     content = session.get(url).content
     soup = BSoup(content, "html.parser")
 
-    News = soup.find_all("div", {"class": "sc-cw4lnv-13 hHSpAQ"})
+    News = soup.find_all("div", {"class": "sc-cw4lnv-12 kQoJyO"})
 
     for article in News:
         main = article.find_all("a", href=True)
@@ -23,7 +23,7 @@ def scrape(request, name):
         linkx = article.find("a", {"class": "sc-1out364-0 dPMosf js_link"})
         link = linkx["href"]
 
-        titlex = article.find("h2", {"class": "sc-759qgu-0 cvZkKd sc-cw4lnv-6 TLSoz"})
+        titlex = article.find("h2", {"class": "sc-759qgu-0 cvZkKd sc-cw4lnv-6 jbPaGl"})
         title = titlex.text
 
         imgx = article.find("img")["data-src"]
